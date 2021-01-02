e5871e7d5a36fe7d47e4631134b9f64d3d98dcd6
Christian Mueller-Hammerstein <mueller-hammerstein@gmx.de>
2021-01-02 17:27:14 +0100

    Change wording in initial question

--- bikeshare.py
+++ bikeshare.py
@@ -23,7 +23,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    print('Would you like to see data for Chicago, New York or Washington?')
+    print('Would you like to see data for Chicago, New York, Washington or for all three cities?')
 
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = ''
